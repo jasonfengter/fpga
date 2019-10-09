d048d4ace755b1f4aaa3f8007da91ee9f04fcf02
Jason Feng <jason.feng.ter@gmail.com>
2019-10-09 21:59:22 +0800

    initial version

---
+++ connected_domain_filter/sim/BRAM_model_rd.v
@@ -0,0 +1,64 @@
+module BRAM_model_rd(
+	i_clk,
+	i_rstn,
+	i_bram_addr,
+	o_bram_data,
+	i_bram_trig,
+	o_bram_done
+	
+);
+	parameter READ_LATENCY=1;  // Defined as: cyc# after 'trig' assertion being accepted
+	input i_clk;
+	input i_rstn;
+	
+	input [12:0] i_bram_addr;
+	output reg [31:0] o_bram_data;
+	input i_bram_trig;
+	output reg o_bram_done;
+	
+	reg [7:0] latency_cnter;
+	
+	always@(posedge i_clk or negedge i_rstn) begin
+		if(~i_rstn) begin
+			o_bram_data <= 32'd0;
+			o_bram_done <= 1'b0;
+			latency_cnter<=8'd0;
+			end
+		else 
+			begin
+				if(i_bram_trig)
+					begin
+						
+						if(latency_cnter==READ_LATENCY)
+							begin
+								o_bram_done<=1'b1;
+								o_bram_data<=return_bram_data(i_bram_addr);
+							end
+						else
+							begin
+								o_bram_done<=1'b0;
+								latency_cnter<=latency_cnter+1'b1;
+								//do not touch o_bram_data
+							end
+					end
+				else
+					begin
+						o_bram_done<=1'b0;
+						latency_cnter<=0;
+					end
+			end
+	
+	end
+	
+	function return_bram_data;
+		input [12:0] addr;
+		begin
+				case(addr)
+					12'h0: return_bram_data=32'h1234_5678;
+					12'h1: return_bram_data=32'h8765_4321;
+					default: return_bram_data=32'hffff_ffff;
+				endcase
+		end
+	endfunction
+
+endmodule
---
+++ connected_domain_filter/sim/tb_rd_512b_from_bram.v
@@ -0,0 +1,80 @@
+`timescale 1ns / 1ps
+
+
+
+module tb_rd_512b_from_bram(
+
+    );
+	
+	// UUT interface
+	reg 				i_clk;
+	reg 				i_rstn;
+	reg 				i_trig;
+	wire 				o_done;
+	reg [8:0] 			i_rd_row_num; // 0-511; 9bit
+	wire [511:0] 		o_rd_data_512b; //512bit
+	wire [12:0] 		o_rd_from_bram_addr; //13bit
+	wire [31:0] 		i_rd_from_bram_data; //32bit
+	wire 				o_rd_from_bram_trig;
+	wire 				i_rd_from_bram_done;
+	
+	// Debug port
+	`define debug_mode
+	wire [7:0] uut_sm_state;
+	
+	
+	rd_512b_from_bram uut(
+		.i_clk(i_clk),
+		.i_rstn(i_rstn),
+		.i_trig(i_trig),
+		.o_done(o_done),
+		.i_rd_row_num(i_rd_row_num), // 0-511, 9bit
+		.o_rd_data_512b(o_rd_data_512b), //512bit
+	// Below to TOP BRAM rd controller
+		.o_rd_from_bram_addr(o_rd_from_bram_addr), //13bit
+		.i_rd_from_bram_data(i_rd_from_bram_data), //32bit
+		.o_rd_from_bram_trig(o_rd_from_bram_trig),
+		.i_rd_from_bram_done(i_rd_from_bram_done),
+		.debug_port({24'd0,uut_sm_state})
+    );
+	
+	BRAM_model_rd BRAM(
+		.i_clk(i_clk),
+		.i_rstn(i_rstn),
+		.i_bram_addr(o_rd_from_bram_addr),
+		.o_bram_data(i_rd_from_bram_data),
+		.i_bram_trig(o_rd_from_bram_trig),
+		.o_bram_done(i_rd_from_bram_done)
+	
+	);
+	
+	// Clock
+	initial i_clk=0;
+	always #1 i_clk = ~i_clk; //500MHz
+	
+	// Reset
+	initial begin
+		i_rstn = 0;
+		#20;
+		@(negedge i_clk) i_rstn=1;
+	end
+	
+	// Set initial condition of uut
+	initial begin
+		i_trig=0;
+		i_rd_row_num=0;
+		#40;
+		@(posedge i_clk) begin
+			i_trig=1;
+			i_rd_row_num=0;
+		end
+		
+		
+	end
+	
+	always@(posedge i_clk) begin
+		if(o_done==1'b1)
+			i_trig<=0;
+	end
+	
+endmodule
